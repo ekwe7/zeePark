f3ba45c86fccf4e6e00621d9824ccc79d5980fd4
ekwe7 <ekwea753@gmail.com>
2026-04-29 22:23:59 +0100

    feat: add BaseDocument and Address common models

---
+++ src/main/java/com/ekwe_hub/zeepark/model/common/Address.java
@@ -0,0 +1,19 @@
+package com.ekwe_hub.zeepark.model.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    private String street;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String country;
+    
+}
