5a9b8c7965767168d7187ae73a54db20b7447355
ekwe7 <ekwea753@gmail.com>
2026-05-04 17:58:42 +0100

    feat: add payment model classes

--- src/main/java/com/ekwe_hub/zeepark/service/AdminService.java
+++ src/main/java/com/ekwe_hub/zeepark/service/AdminService.java
@@ -3,6 +3,7 @@ package com.ekwe_hub.zeepark.service;
 import com.ekwe_hub.zeepark.dto.request.ParkingZoneRequest;
 import com.ekwe_hub.zeepark.dto.request.SpotCategoryRequest;
 import com.ekwe_hub.zeepark.dto.response.RevenueReportResponse;
+import com.ekwe_hub.zeepark.model.parking.ParkingSpot;
 import com.ekwe_hub.zeepark.model.parking.ParkingZone;
 import com.ekwe_hub.zeepark.model.parking.SpotCategory;
 
@@ -11,6 +12,9 @@ import java.util.List;
 
 public interface AdminService {
     ParkingZone createZone(ParkingZoneRequest request);
+    void deleteZone(String zoneId);
+    ParkingSpot createSpot(String zoneId, String categoryId);
+    List<ParkingSpot> findAllSpots();
     List<ParkingZone> findAllZones();
     SpotCategory createSpotCategory(SpotCategoryRequest request);
     List<SpotCategory> findAllSpotCategories();
